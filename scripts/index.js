fb9ac3c798532a65236ff6461b90af2d854d3dc4
catnnguyen <catnguyen805@gmail.com>
2024-04-26 10:14:33 -0700

    timer

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'April 22, 2024',
+  birthdate: 'May 01, 2024',
   name: 'ALEX!!!'
 };
 
